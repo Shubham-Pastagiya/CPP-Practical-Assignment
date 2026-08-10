704bcb45a6deec0afc64b6de45e847e9aa358274
Shubham11-1055 <payalpastagiya11@gamil.com>
2026-08-10 08:52:08 +0530

    Modified P2.cpp

--- Unit 1/p2.cpp
+++ Unit 1/p2.cpp
@@ -1,8 +1,46 @@
-#include<iostream>
+// Write a C++ program to create a class Circle with private data
+// member radius. Provide a member function to calculate and return
+// the area of the circle.
+
+#include <iostream>
 using namespace std;
-int main()
+class Circle
 {
-    cout << "Hello World\n";
+    private:
+    float radius,area;
+    public:
+    void input()
+    {
+        cout<<"Enter Radius: ";
+        cin>>radius;
+    }
+
+    void Calc()
+    {
+        area = 3.14*radius*radius;
+    }
+
+    void dis()
+    {
+        cout<<"Area Of Circle: "<<area<<endl;
+    }
+};
+void clearScreen()
+{
+    #ifdef _WIN32
+    system("cls");
+    #else
+    system("clear");
+    #endif
 }
 
 
+int main()
+{
+    clearScreen();
+    Circle c1;
+    c1.input();
+    c1.Calc();
+    c1.dis();
+    return 0;
+}
