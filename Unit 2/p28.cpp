f023bb62ecf10a67cfd99f360a8b85b1c8216388
Shubham11-1055 <payalpastagiya11@gamil.com>
2026-09-03 09:18:30 +0530

    p28.cpp modified

--- Unit 2/p28.cpp
+++ Unit 2/p28.cpp
@@ -4,9 +4,27 @@
 
 #include <iostream>
 using namespace std;
-
+class Complex
+{
+    int real,imag;
+    public:
+        void set(int r,int i)
+        {
+            real = r;
+            imag = i;
+        }
+        void display()
+        {
+            cout<<"Complex Number: "<<real<<" + "<<imag;
+        }
+};
 int main()
 {
+    Complex *ptr = new Complex;
 
+    ptr->set(5,3);
+    ptr->display();
+
+    delete ptr;
     return 0;
 }
